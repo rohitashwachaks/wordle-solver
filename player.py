8306a09b4f11088a198e7a0b404333658cd2388f
CaptainNemo <rohitashwachaks@gmail.com>
2022-02-10 12:41:26 -0600

    update

--- player.py
+++ player.py
@@ -23,6 +23,8 @@ class Player:
 
         self._board_response = []
 
+        self.information_gain_history = []
+
         return
     @property
     def board_response(self):
@@ -38,13 +40,16 @@ class Player:
             else:
                 return (f'win at turn {response}')
         
-        response= np.array(response)
+        # response = np.array(response)
 
         for index, res in enumerate(response):
             if res == 'G':
                 # print(colored(self.guessed_word[index], "green"))
                 self.vocab = self.vocab[self.vocab[index] == self.guessed_word[index]]
             elif res == 'B':
+                if self.guessed_word[index] in self.guessed_word.iloc[np.where(response == 'G')]:
+                    print("here")
+                    continue
                 # print(colored(self.guessed_word[index], "red"))
                 self.vocab = self.vocab[self.vocab.apply(lambda x: (self.guessed_word[index] not in ''.join(x)), axis= 1)]
             elif res =='Y':
@@ -54,19 +59,37 @@ class Player:
         self.vocab.reset_index(drop= True, inplace= True)
         return
 
-    def guess(self):
+    def information_gain(self):
         probability = self.vocab.apply(pd.value_counts)
+        
         probability = probability/probability.sum()
         
         information = -probability*np.log2(probability)
         
         information = information.to_dict()
 
-        self.guessed_word = self.vocab.iloc[self.vocab.apply(lambda x: ([information[x.name][i] for i in x])).sum(axis=1).idxmax()]
+        information_df = pd.DataFrame(self.vocab.apply(lambda x: ([information[x.name][i] for i in x])).sum(axis=1), columns= ['information'])
+        information_df['word'] = self.vocab.apply(lambda x: (''.join(x)), axis= 1)
+        
+        return information_df
+        
+
+    def guess(self):
+        self.information_df = self.information_gain().sort_values(by= ['information'], ascending= False).reset_index(drop= True)
+
+        if self.verbose:
+            print(self.information_df)
+        # if self.guess_number == 0:
+
+        
+        information, self.guessed_word = self.information_df.iloc[ self.information_df['information'].idxmax()].to_list()
+        self.information_gain_history.append(np.round(information, 3))
         
         if self.verbose:
-            print(f"information gain: {np.round(self.vocab.apply(lambda x: ([information[x.name][i] for i in x])).sum(axis=1).max(),3)}")
+            print(f"information gain: {self.information_gain_history[-1]}")
         
+        self.guess_number += 1
+        self.guessed_word = pd.Series(list(self.guessed_word))
         return ''.join(self.guessed_word)
     
     def render(self):
